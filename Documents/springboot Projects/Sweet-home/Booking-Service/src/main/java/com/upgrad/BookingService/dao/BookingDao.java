ad2313f75df487766f5f4dbfa9835978e70fbfff
Mwajahath <62271760+Mwajahath@users.noreply.github.com>
2022-04-10 08:15:52 +0530

    Delete Documents/springboot Projects/Sweet-home directory

--- Documents/springboot Projects/Sweet-home/Booking-Service/src/main/java/com/upgrad/BookingService/dao/BookingDao.java
+++
@@ -1,9 +0,0 @@
-package com.upgrad.BookingService.dao;
-
-import com.upgrad.BookingService.entity.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingDao extends JpaRepository<Booking,Integer> {
-}
--- Documents/springboot Projects/Sweet-home/Payment-Service /src/main/java/com/upgrad/PaymentService/service/PaymentService.java
+++
@@ -1,8 +0,0 @@
-package com.upgrad.PaymentService.service;
-
-import com.upgrad.PaymentService.entity.Payment;
-
-public interface PaymentService {
-
-    Payment getPaymentById(int paymentId) throws Exception;
-}
