08454246d0d8933ef0a4da7b41456277665738dd
Mwajahath <wajahath845@gmail.com>
2022-04-10 08:11:28 +0530

    first commit

---
+++ Documents/C3_Project-master/src/main/java/Restaurant.java
@@ -0,0 +1,87 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Restaurant {
+    private String name;
+    private String location;
+    public LocalTime openingTime;
+    public LocalTime closingTime;
+    private List<Item> menu = new ArrayList<Item>();
+
+    public Restaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.location = location;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
+
+    public boolean isRestaurantOpen() {
+        LocalTime localTime = getCurrentTime();
+        boolean open = localTime.isAfter(openingTime);
+        System.out.println(open);
+        boolean close = localTime.isBefore(closingTime);
+        System.out.println(close);
+        return open && close;
+        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+    }
+
+    public LocalTime getCurrentTime(){ return  LocalTime.now(); }
+
+    public List<Item> getMenu() {
+        return menu;
+        //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
+    }
+
+    private Item findItemByName(String itemName){
+        for(Item item: menu) {
+            if(item.getName().equals(itemName))
+                return item;
+        }
+        return null;
+    }
+
+    public void addToMenu(String name, int price) {
+        Item newItem = new Item(name,price);
+        menu.add(newItem);
+    }
+    
+    public void removeFromMenu(String itemName) throws itemNotFoundException {
+
+        Item itemToBeRemoved = findItemByName(itemName);
+        if (itemToBeRemoved == null)
+            throw new itemNotFoundException(itemName);
+
+        menu.remove(itemToBeRemoved);
+    }
+    public void displayDetails(){
+        System.out.println("Restaurant:"+ name + "\n"
+                +"Location:"+ location + "\n"
+                +"Opening time:"+ openingTime +"\n"
+                +"Closing time:"+ closingTime +"\n"
+                +"Menu:"+"\n"+getMenu());
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTotalItemPrice(List<String> itemNames){
+        int totalPrice = 0;
+        for(String itemName:itemNames){
+            Item item = findItemByName(itemName);
+            if(item!=null){
+                String itemString = item.toString();
+                int first = itemString.indexOf(':');
+                int last = itemString.indexOf('\n');
+                String strPrice = itemString.substring(first+1,last);
+                System.out.println("String Price is"+strPrice);
+                int price = Integer.parseInt(strPrice);
+                totalPrice = totalPrice + price;
+            }
+        }
+        return totalPrice;
+    }
+}
